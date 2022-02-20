a4641688d3a80deb5b62175d890113dd2af385f2
Chris Stone <contact@chrisstone.dev>
2022-02-20 13:11:22 -0500

    Add Lumen Bootswatch theme

--- VideoRentalService/App_Start/BundleConfig.cs
+++ VideoRentalService/App_Start/BundleConfig.cs
@@ -22,7 +22,7 @@ namespace VideoRentalService
                 "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-lumen.css",
                 "~/Content/site.css"));
         }
     }
