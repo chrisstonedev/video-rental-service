6bb9928819210f0e9112c0e93fd382a8a711da82
Chris Stone <contact@chrisstone.dev>
2022-02-20 13:37:47 -0500

    Add new route and several methods for movies controller

--- VideoRentalService/App_Start/RouteConfig.cs
+++ VideoRentalService/App_Start/RouteConfig.cs
@@ -10,6 +10,13 @@ namespace VideoRentalService
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new {controller = "Movies", action = "ByReleaseDate"},
+                new {year = @"\d{4}", month = @"\d{2}"}
+            );
+
+            routes.MapRoute(
                 "Default",
                 "{controller}/{action}/{id}",
                 new {controller = "Home", action = "Index", id = UrlParameter.Optional}
--- VideoRentalService/Controllers/MoviesController.cs
+++ VideoRentalService/Controllers/MoviesController.cs
@@ -13,5 +13,25 @@ namespace VideoRentalService.Controllers
             };
             return View(movie);
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content($"id={id}");
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}/{month}");
+        }
     }
 }
